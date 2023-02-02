ff1aab9adbbd547b4b655ba9dd35a4221a4129b7
Sam Ferree <sferree567@gmail.com>
2023-02-02 12:18:56 -0500

    remove time tracking.

--- PokerHandKata.Web/Program.cs
+++ PokerHandKata.Web/Program.cs
@@ -9,11 +9,3 @@ app.Urls.Add("https://localhost:7020");
 app.MapControllers();
 
 app.Run();
-
-// Time spent on app
-// 2023-01-23: 45 minutes
-// 2023-01-24: 60 minutes
-// 2023-01-25: 75 minutes
-// 2023-01-27: 45 minutes
-// 2023-01-30: 60 minutes
-// 2023-02-01: 60 minutes
